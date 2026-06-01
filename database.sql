f639375d851cfbba13ee419e77f5d1692d042b1e
mariemnofal46-create <mariemnofal46@gmail.com>
2026-06-01 14:39:33 +0300

    Add database file

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    grade TEXT,
+    department TEXT,
+    city TEXT
+);
+
+INSERT INTO Students VALUES (1, 'Mariam', 20, 'A', 'Computer Science', 'Alex');
+INSERT INTO Students VALUES (2, 'Sara', 19, 'A', 'Computer Science', 'Alex');
+INSERT INTO Students VALUES (3, 'Ali', 21, 'B', 'IT', 'Cairo');
+INSERT INTO Students VALUES (4, 'Laila', 18, 'A', 'IT', 'Cairo');
+INSERT INTO Students VALUES (5, 'Omar', 22, 'C', 'Engineering', 'Giza');
+INSERT INTO Students VALUES (6, 'Hassan', 21, 'C', 'Engineering', 'Alex');
